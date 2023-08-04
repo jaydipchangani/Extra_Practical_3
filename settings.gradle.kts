fcaacf6d5ec10bccdd4b320cc6a28acfd9a9ef1e
student <bhargavdodiya21@gnu.ac.in>
2023-08-04 08:59:11 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Extra_Practical_3"
+include(":app")
